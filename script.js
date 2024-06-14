72db672a1e8a250e3f07da40f96ae77ce48e4938
Isaacmrx <151270600+Isaacmrx@users.noreply.github.com>
2024-06-14 15:12:11 -0600

    Update script.js
    agregar un emoji

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLEAÑOS', ' VALEEE!' ],
+			strings: [ 'FELIZ', 'CUMPLEAÑOS', ' VALEEE!❤️' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
